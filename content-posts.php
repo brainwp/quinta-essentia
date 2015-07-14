d89fcb5c574af26eb065abc4deb720b0af814663
Bruno Magrini <brmagrini@gmail.com>
2015-07-14 18:18:33 -0300

    muda estilo blog

---
+++ content-posts.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The default template for displaying content.
+ *
+ * Used for both single and index/archive/author/catagory/search/tag.
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+
+
+
+?>
+
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+				the_title( '<h1 class="entry-title">', '</h1>' );
+		?>
+
+	</header><!-- .entry-header -->
+
+		<div id="page-content">
+			<?php
+				the_content( );?>	
+		</div><!-- #page-content -->
+
+
+	
+</article><!-- #post-## -->
---
+++ page-Blog.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template Name: Blog
+ *
+ * .
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+
+get_header('page'); ?>
+
+	<div id="primary" class="<?php echo odin_classes_page_sidebar(); ?>">
+		<main id="main-content" class="site-main" role="main">
+				<?php 
+
+			$args = array(
+				'post_type' => 'post',
+				// 'meta_key' => 'ordem',
+				// 			'orderby' => 'meta_value',
+				// 			'order'   => 'ASC'
+			);
+			
+			$query = new WP_Query( $args );			// the query
+				$query_secao = new WP_Query( $args ); ?>
+
+				<?php if ( $query_secao->have_posts() ) : ?>
+
+					<!-- pagination here -->
+
+					<!-- the loop -->
+					<?php while ( $query_secao->have_posts() ) : $query_secao->the_post(); 
+
+							get_template_part( 'content', 'posts' );						
+
+						endwhile; ?>
+					<!-- end of the loop -->
+
+					<!-- pagination here -->
+
+					<?php wp_reset_postdata(); ?>
+
+				<?php endif; 
+				?>
+			
+			
+			
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	get_sidebar();
+	
+<?php
+get_footer('page');
