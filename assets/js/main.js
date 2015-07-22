a2e47cdf3298396f4405c06f022c2e7e5d3e4230
Bruno Magrini <brmagrini@gmail.com>
2015-07-22 02:11:11 -0300

    segue com fitvids e span

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,6 +76,8 @@ jQuery(document).ready(function($) {
 	
 	// fitVids.
 	$( '.entry-content' ).fitVids();
+	$( '.single #main-content' ).fitVids();
+	
 
 	// Responsive wp_video_shortcode().
 	$( '.wp-video-shortcode' ).parent( 'div' ).css( 'width', 'auto' );
