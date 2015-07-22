cf2f077c597b3e8c518e9a1671e83a21d787200d
Bruno Magrini <brmagrini@gmail.com>
2015-07-22 01:51:48 -0300

    verificar

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,8 +76,6 @@ jQuery(document).ready(function($) {
 	
 	// fitVids.
 	$( '.entry-content' ).fitVids();
-	$( '.single #main-content' ).fitVids();
-	
 
 	// Responsive wp_video_shortcode().
 	$( '.wp-video-shortcode' ).parent( 'div' ).css( 'width', 'auto' );
